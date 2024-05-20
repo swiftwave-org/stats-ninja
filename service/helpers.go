318a6e59f29e153f57f6566632231aacce38dc0c
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-05-20 22:50:24 +0530

    feat: instead of cpu percentage send cpu time

--- service/helpers.go
+++ service/helpers.go
@@ -6,16 +6,6 @@ import (
 	"github.com/docker/docker/api/types"
 )
 
-func calculateCPUPercentUnix(v *types.StatsJSON) uint8 {
-	/*
-		cpu_delta = cpu_stats.cpu_usage.total_usage - precpu_stats.cpu_usage.total_usage
-		system_cpu_delta = cpu_stats.system_cpu_usage - precpu_stats.system_cpu_usage
-		CPU usage % = (cpu_delta / system_cpu_delta) * 100.0
-	*/
-	cpuDelta := float64(v.CPUStats.CPUUsage.TotalUsage) - float64(v.PreCPUStats.CPUUsage.TotalUsage)
-	systemDelta := float64(v.CPUStats.SystemUsage) - float64(v.PreCPUStats.SystemUsage)
-	return uint8(math.Round((cpuDelta / systemDelta) * 100.0))
-}
 
 func memoryUsageMB(v *types.StatsJSON) uint64 {
 	// used_memory = memory_stats.usage - memory_stats.stats.cache
--- service/types.go
+++ service/types.go
@@ -1,12 +1,17 @@
 package service
 
 type ResourceStats struct {
-	CpuUsagePercent uint8   `json:"cpu_used_percent"`
-	UsedMemoryMB    uint64  `json:"used_memory_mb"`
-	NetStat         NetStat `json:"network"`
+	CpuTime      *CpuTime `json:"cpu_usage_stat"`
+	UsedMemoryMB uint64   `json:"used_memory_mb"`
+	NetStat      *NetStat `json:"network"`
 }
 
 type NetStat struct {
 	SentKB uint64 `json:"sent_kb"`
 	RecvKB uint64 `json:"recv_kb"`
+}
+
+type CpuTime struct {
+	Service uint64 `json:"application"`
+	System  uint64 `json:"system"`
 }
